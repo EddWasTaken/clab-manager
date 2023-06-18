da13e1d6426fa12a7bd06886ee5baf8f39f59340
Gerson Fernandes <2200680@my.ipleiria.pt>
2023-06-18 20:36:12 +0100

    Frontend generator + cenario

--- frontend/src/scenes/cenario/cenario.jsx
+++ frontend/src/scenes/cenario/cenario.jsx
@@ -1,4 +1,5 @@
-import { Box } from "@mui/material";
+import React, { useState } from "react";
+import { Box, TextField, Button } from "@mui/material";
 import { useLocation } from "react-router-dom";
 import Header from "../../components/Header";
 
@@ -6,7 +7,12 @@ const Cenario = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const port = searchParams.get("port");
-  console.log(port);
+  const [secondBoxIp, setSecondBoxIp] = useState(""); 
+  const [connected, setConnected] = useState(false); 
+
+  const handleConnect = () => {
+    setConnected(true); // faz aparecer a imagem
+  };
 
   return (
     <Box m="20px">
@@ -14,12 +20,35 @@ const Cenario = () => {
         <Header title="Cenario" subtitle="Welcome to your Cenario" />
       </Box>
 
-      <Box mt="20px">
-        <iframe
-        //atenção ao IP -> Depois Mudar ################3
-          src={`http://192.168.83.211:${port}/`}
-          style={{ width: "70%", height: "700px", border: "none" }}
+      <Box display="flex" alignItems="center" mt="20px">
+        <TextField
+          label="Second Box IP Address"
+          value={secondBoxIp}
+          onChange={(e) => setSecondBoxIp(e.target.value)}
+          variant="outlined"
+          margin="dense"
         />
+        <Button variant="contained" onClick={handleConnect} sx={{ ml: "10px" }}>
+          Connect
+        </Button>
+      </Box>
+
+      <Box display="flex" mt="20px">
+        <Box flex="1" mr="10px">
+          <iframe
+            src={`http://192.168.83.211:${port}/`}
+            style={{ width: "100%", height: "700px", border: "none" }}
+          />
+        </Box>
+
+        {connected && (
+          <Box flex="1" ml="10px">
+            <iframe
+              src={`http://${secondBoxIp}/`}
+              style={{ width: "100%", height: "700px", border: "none" }}
+            />
+          </Box>
+        )}
       </Box>
     </Box>
   );
--- frontend/src/scenes/generator/generator.jsx
+++ frontend/src/scenes/generator/generator.jsx
@@ -1,6 +1,6 @@
 import yaml from "js-yaml";
 import React, { useState } from "react";
-import { Box, TextField, Button } from "@mui/material";
+import { Box, TextField, Button, Select, MenuItem } from "@mui/material";
 import Header from "../../components/Header";
 
 const Generator = () => {
@@ -10,10 +10,48 @@ const Generator = () => {
   const [topology, setYaml] = useState("");
 
   const handleAddNode = () => {
-    setNodes([...nodes, { kind: "", image: "", ports: [] }]);
+    setNodes([...nodes, { selectedOption: "Select Server", kind: "", image: "", ports: [] }]);
   };
 
   const handleNodeChange = (index, field, value) => {
+    const updatedNodes = [...nodes];
+    updatedNodes[index][field] = value;
+    setNodes(updatedNodes);
+  };
+
+  const handleKindImageChange = (index, value) => {
+    const updatedNodes = [...nodes];
+    updatedNodes[index].selectedOption = value;
+
+    switch (value) {
+      case "SR Linux":
+        updatedNodes[index].kind = "srl";
+        updatedNodes[index].image = "ghcr.io/nokia/srlinux";
+        break;
+      case "Router Ceos":
+        updatedNodes[index].kind = "ceos";
+        updatedNodes[index].image = "ceos:4.30.0F";
+        break;
+      case "Server Nginx":
+        updatedNodes[index].kind = "linux";
+        updatedNodes[index].image = "nginx:latest";
+        break;
+      case "Linux Desktop":
+        updatedNodes[index].kind = "linux";
+        updatedNodes[index].image = "dorowu/ubuntu-desktop-lxde-vnc";
+        break;
+      case "Other":
+        updatedNodes[index].kind = "";
+        updatedNodes[index].image = "";
+        break;
+      default:
+        break;
+    }
+
+    setNodes(updatedNodes);
+  };
+
+  const handleKindImageInputChange = (index, field, value) => {
     const updatedNodes = [...nodes];
     updatedNodes[index][field] = value;
     setNodes(updatedNodes);
@@ -50,37 +88,32 @@ const Generator = () => {
   };
 
   const postDeploy = async () => {
-
     const jsonObj = yaml.load(topology);
     const jsonStr = JSON.stringify(jsonObj);
 
     try {
-      const response = await fetch("/api/topologies/", {
+      const response = await fetch("http://localhost:5000/api/topologies", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: jsonStr,
-      
       });
       if (response.ok) {
-        
         console.log("Deploy successful");
       } else {
-       
         console.error("Deploy failed");
       }
     } catch (error) {
       console.error("Error occurred during deployment:", error);
     }
   };
-  
 
   return (
     <Box m="20px" display="flex">
       <Box flex="1">
         <Box display="flex" justifyContent="space-between" alignItems="center">
-          <Header title="YAML Generator"/>
+          <Header title="YAML Generator" />
         </Box>
         <Box mt="20px">
           <TextField
@@ -93,22 +126,41 @@ const Generator = () => {
           <Button variant="contained" onClick={handleAddNode}>
             Add Node
           </Button>
+          {/*  DROP LIST */}
           {nodes.map((node, index) => (
             <Box key={index} mt="20px">
-              <TextField
-                label="Node Kind"
-                value={node.kind}
-                onChange={(e) => handleNodeChange(index, "kind", e.target.value)}
+              <Select
+                onChange={(e) => handleKindImageChange(index, e.target.value)}
                 fullWidth
                 margin="normal"
-              />
-              <TextField
-                label="Node Image"
-                value={node.image}
-                onChange={(e) => handleNodeChange(index, "image", e.target.value)}
-                fullWidth
-                margin="normal"
-              />
+                value={node.selectedOption}
+              >
+                <MenuItem value="Select Server">Select Server</MenuItem>
+                <MenuItem value="SR Linux">SR Linux</MenuItem>
+                <MenuItem value="Router Ceos">Router Ceos</MenuItem>
+                <MenuItem value="Server Nginx">Server Nginx</MenuItem>
+                <MenuItem value="Linux Desktop">Linux Desktop</MenuItem>
+                <MenuItem value="Other">Other</MenuItem>
+                
+              </Select>
+              {node.selectedOption === "Other" && (
+                <>
+                  <TextField
+                    label="Node Kind"
+                    value={node.kind}
+                    onChange={(e) => handleKindImageInputChange(index, "kind", e.target.value)}
+                    fullWidth
+                    margin="normal"
+                  />
+                  <TextField
+                    label="Node Image"
+                    value={node.image}
+                    onChange={(e) => handleKindImageInputChange(index, "image", e.target.value)}
+                    fullWidth
+                    margin="normal"
+                  />
+                </>
+              )}
               <TextField
                 label="Node Ports"
                 value={node.ports.join(",")}
@@ -121,6 +173,7 @@ const Generator = () => {
           <Button variant="contained" onClick={handleAddLink} mt="20px">
             Add Link
           </Button>
+          
           {links.map((link, index) => (
             <Box key={index} mt="20px">
               <TextField
@@ -132,13 +185,12 @@ const Generator = () => {
               />
             </Box>
           ))}
-          
         </Box>
       </Box>
       <Box flex="1" ml="20px">
-      <Button variant="contained" onClick={generateYaml} mt="20px">
-            Generate YAML
-          </Button>
+        <Button variant="contained" onClick={generateYaml} mt="20px">
+          Generate YAML
+        </Button>
         <Button variant="contained" onClick={postDeploy} mt="20px">
           Save
         </Button>
@@ -147,7 +199,6 @@ const Generator = () => {
             <pre>{topology}</pre>
           </Box>
         )}
-        
       </Box>
     </Box>
   );
